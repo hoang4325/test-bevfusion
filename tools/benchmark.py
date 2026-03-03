ab6b1a1e2686904e8c99ce0a71a48dc00d532fb5
rathaROG <10178493+rathaROG@users.noreply.github.com>
2026-03-03 04:59:32 +0100

    Add dirty_patches to benchmark.py

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -1,6 +1,9 @@
 import argparse
 import time
 import torch
+
+import dirty_patches
+
 from mmcv import Config
 from mmcv.parallel import MMDataParallel
 from mmcv.runner import load_checkpoint, wrap_fp16_model
